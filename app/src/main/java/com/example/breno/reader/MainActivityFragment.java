463fee28e67827f52227265f88cea73963a35534
Breno Souza <brenosamuelaraujodesouza@gmail.com>
2017-02-28 03:12:27 -0300

    Adicionando Layout Inicial

--- app/src/main/java/com/example/breno/reader/MainActivityFragment.java
+++ app/src/main/java/com/example/breno/reader/MainActivityFragment.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +24,34 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Criando um array falsa de strings com os titulos das nossas Aulas
+        String[] data= {
+                "Aula 1: Getting Start!",
+                "Aula 2: Como instalar o Android Studio?",
+                "Aula 3: Como criar um projeto no Android Studio?",
+                "Aula 4: O que é Activity e Fragment?",
+                "Aula 5: Como criar a User Interface (UI) do meu Aplicativo Android?"
+        };
+
+        //Criando uma lista (ArrayList) com os dados acima
+        List<String> listOfLastPosts = new ArrayList<String>(Arrays.asList(data));
+
+        //Agora que já temos os dados, vamos criar um Adapter, no caso um ArrayAdapter
+        ArrayAdapter<String> listOfLastPostsAdapter = new ArrayAdapter<String>(
+                getActivity(),//Contexto Atual
+                R.layout.list_item_last_posts, // O arquivo layout de cada item
+                R.id.list_item_post_title_textview, //O id do campo a ser preenchido
+                listOfLastPosts // A fonte dos dados
+        );
+        //Inflamos o layout principal
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Cria uma referência para a ListView
+        ListView listView = (ListView) rootView.findViewById(R.id.list_last_posts);
+        listView.setAdapter(listOfLastPostsAdapter);
+
+        //Retornamos tudo
+        return rootView;
     }
 }
